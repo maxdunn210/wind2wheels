aabb4573368479b6d24da535968e5ad6af2738b4
Max Dunn <max@maxdunn.com>
2014-04-26 17:01:36 -0700

    Charging algorithm working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+
+  # MD Apr-2014 Turn this off for now because of ajax post
+  # protect_from_forgery with: :exception
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   resources :users
   resources :cars
   get 'utility' => 'utility#index'
+  post 'utility/change' => 'utility#change'
+
   get 'car_app' => 'car_app#index'
+  get 'car_app/charging' => 'car_app#charging'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
